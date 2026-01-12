3b755a5ced6a1dc6d3a9aa2377f3ade072278326
Oshaq Naimat <oshaqnaimat15@gmail.com>
2026-01-12 20:30:09 +0500

    sidebar

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Sidebar = () => {
-  return <div></div>;
+  return (
+    <div className="bg-[#192237] min-h-screen col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3"></div>
+  );
 };
 
 export default Sidebar;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Sidebar from './components/Sidebar'
 
 const page = () => {
   return (
-    <div>
-      <h1>this is global page </h1>
+    <div className='grid grid-cols-12'>
+      <Sidebar/>
     </div>
   )
 }
