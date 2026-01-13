8656e0c08a5c4e0b684802eee7888cef52d7214d
Oshaq Naimat <oshaqnaimat15@gmail.com>
2026-01-13 11:47:15 +0500

    navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { FaBell, FaChair, FaGlobe } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>this is navabr</h1>
+    <div className="w-[90%] mx-auto my-5 text-white  bg-[#192237] p-4 rounded-md flex justify-between ">
+      <h2 className="text-xl">Dashboard</h2>
+      <div className="flex items-center gap-3">
+        <input
+          type="text"
+          placeholder="Search.."
+          className="bg-[#192237] p-2 rounded-md"
+        />
+        <FaBell />
+        <FaGlobe />
+        <FaChair />
+      </div>
     </div>
   );
 };
