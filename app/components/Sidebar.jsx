1ea3083dfdd9ebdb64b3b409f7895c76c58a91a6
Oshaq Naimat <oshaqnaimat15@gmail.com>
2026-01-17 14:12:48 +0500

    dynamic

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,41 +1,47 @@
 import React from "react";
-import { IoMdPerson } from "react-icons/io";
-import { sidebarData } from "../data/SidebarData";
+import { FaUser } from "react-icons/fa";
+import { sidebarData } from "../data/sidebarData";
+import Link from "next/link";
 
 const Sidebar = () => {
   return (
-    <div className="bg-[#192237] sticky left-0 h-[90vh]  overflow-y-scroll min-h-screen col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 xl:col-span-2">
-      <div className="flex text-white  items-center p-5 gap-2 hover:scale-95 hover:bg-gray-900 rounded-md duration-100 transition cursor-pointer">
-        <IoMdPerson className="w-10 h-10 rounded-full bg-gray-500 p-3" />
-        <div className="flex flex-col">
-          <p className="font-bold text-md">Username</p>
-          <span className="text-gray-300 font-light">Admin</span>
+    <>
+      <div className="min-h-screen col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 bg-[#192237] xl:col-span-2 p-5">
+        <div className="flex gap-4">
+          <div className="h-10 flex justify-center items-center w-10 rounded-full bg-gray-300">
+            <FaUser />
+          </div>
+          <div className="text-white">
+            <h3 className="font-bold">Username</h3>
+            <p className="text-gray-500">Role</p>
+          </div>
         </div>
-      </div>
 
-      <ul className="flex flex-col gap-3">
-        {sidebarData.map((item, index) => {
-          return (
-            <li className="text-gray-500 font-semibold" key={index}>
-              {item.section}
-              <ul>
-                {item.Lists.map((item, index) => {
-                  return (
-                    <li
-                      className="text-white items-center gap-2 ms-5 my-2 hover:bg-gray-900 p-2 rounded-md cursor-pointer hover:scale-95 duration-100 transition font-semibold flex"
-                      key={index}
-                    >
-                      {item.icon}
-                      {item.name}
-                    </li>
-                  );
-                })}
-              </ul>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+        <ul className="flex flex-col gap-3">
+          {sidebarData.map((item, index) => {
+            return (
+              <li className="text-gray-300 font-semibold" key={index}>
+                {item.name}
+
+                <ul className="flex ms-4 flex-col gap-3">
+                  {item.list.map((item2, index2) => {
+                    return (
+                      <Link
+                        href={item2.path}
+                        className="text-gray-300 py-3 hover:bg-gray-900 px-3 flex gap-3 items-center font-semibold"
+                        key={index2}
+                      >
+                        {item2.icon} {item2.name}
+                      </Link>
+                    );
+                  })}
+                </ul>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
 };
 
