4ce108936f1a5da8b9c70fffc57f5d4cb70f036b
Oshaq Naimat <oshaqnaimat15@gmail.com>
2026-01-17 13:52:27 +0500

    dynamic

--- app/backend/actions/userActions.js
+++ app/backend/actions/userActions.js
@@ -1,21 +1,26 @@
-import { revalidatePath } from "next/cache"
+"use server"
+
 import { redirect } from "next/navigation"
-
-export const addData  = async(formData)=>{
-    const {username,email,password,phone,status,role,comments} = Object.fromEntries(formData)
-
-
-    const newUser = await User.create({
-        username,email,password,phone,status,role,comments
-    })
+import { User } from "../models/userModel"
+import { revalidatePath } from "next/cache"
 
 
-    redirect('/dashboard/users/show-user')
+
+export const addData = async ( formData ) => {
+    const { username, email, password, phone, status, role, comments } = Object.fromEntries( formData )
+
+    const newUser = await User.create( {
+        username, email, password, phone, status, role, comments
+    } )
+
+
+    redirect( '/dashboard/users/show-user' )
+
+
 }
 
-
-export const deleteData = async(formData)=>{
-    let id = formData.get('id')
-    let deleteData = await User.findByIdAndDelete(id)
-    revalidatePath('/dashboard/users/show-user')
+export const deleteData = async ( formData ) => {
+    let id = formData.get( 'id' )
+    let deleteUser = await User.findByIdAndDelete( id )
+    revalidatePath( '/dashboard/users/show-user' )
 }
--- app/dashboard/user/page.jsx
+++ app/dashboard/user/page.jsx
@@ -1,7 +1,81 @@
+import { addData } from "@/app/backend/actions/userActions";
+import { addUser } from "@/app/backend/actions/userActions";
+import Button from "@/app/components/Button";
 import React from "react";
 
 const page = () => {
-  return <div>this is a User page page</div>;
+  return (
+    <>
+      <form action={addData} className="p-5 bg-[#192237]">
+        <div className="flex mb-3 gap-3">
+          <input
+            name="username"
+            type="text"
+            className="bg-[#151C2D] w-full outline-0 p-3 placeholder:text-gray-400 text-gray-400"
+            placeholder="Username"
+          />
+          <input
+            name="email"
+            type="email"
+            className="bg-[#151C2D] w-full outline-0 p-3 placeholder:text-gray-400 text-gray-400"
+            placeholder="Email"
+          />
+        </div>
+        <div className="flex mb-3 gap-3">
+          <input
+            name="password"
+            type="password"
+            className="bg-[#151C2D] w-full outline-0 p-3 placeholder:text-gray-400 text-gray-400"
+            placeholder="Password"
+          />
+          <input
+            name="phone"
+            type="text"
+            className="bg-[#151C2D] w-full outline-0 p-3 placeholder:text-gray-400 text-gray-400"
+            placeholder="Phone"
+          />
+        </div>
+        <div className="flex mb-3 gap-3">
+          <select
+            name="role"
+            id=""
+            className="bg-[#151C2D] w-full outline-0 p-3 placeholder:text-gray-400 text-gray-400"
+          >
+            <option selected disabled>
+              Select Role
+            </option>
+            <option value="1">Admin</option>
+            <option value="0">User</option>
+          </select>
+          <select
+            name="status"
+            id=""
+            className="bg-[#151C2D] w-full outline-0 p-3 placeholder:text-gray-400 text-gray-400"
+          >
+            <option selected disabled>
+              Select Status
+            </option>
+            <option value="1">Active</option>
+            <option value="0">Inactive</option>
+          </select>
+        </div>
+        <textarea
+          rows={5}
+          placeholder="Comments..."
+          className="bg-[#151C2D] w-full outline-0 p-3 placeholder:text-gray-400 text-gray-400"
+          name="comments"
+          id=""
+        ></textarea>
+
+        <Button
+          bg="bg-green-500"
+          loadingBg={"bg-gray-500"}
+          content={"Add user"}
+          loadingText={"Adding..."}
+        />
+      </form>
+    </>
+  );
 };
 
 export default page;
